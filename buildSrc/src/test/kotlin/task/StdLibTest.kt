7abdd62772941235ea0883152e8e7c4c23c64d34
Philippe Kernevez <philippe@kernevez.net>
2023-04-12 15:09:16 +0200

    Import ef30

--- buildSrc/src/test/kotlin/task/StdLibTest.kt
+++ buildSrc/src/test/kotlin/task/StdLibTest.kt
@@ -1,16 +1,11 @@
 package task
 
 import org.apache.commons.csv.CSVFormat
-import org.apache.commons.csv.CSVParser
 import org.apache.commons.csv.CSVRecord
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
-import java.io.FileInputStream
 import java.io.FileReader
 import java.io.InputStream
-import java.nio.charset.Charset
-import kotlin.streams.asSequence
 
 
 class StdLibTest {
@@ -67,12 +62,13 @@ class StdLibTest {
     }
 
     @Test
-    fun substance() {
+    fun substance31() {
         // given
-        val substances = loadRecords()
+        val substances = loadRecords("31")
+            .map { EF31Record(it) }
             .groupingBy { it.substanceId() }
-            .fold({ _: String, element: CSVRecord -> Impact() },
-                { _: String, accumulator: Impact, element: CSVRecord -> accumulator.factor(element)})
+            .fold({ _: String, element: EFRecord -> Impact() },
+                { _: String, accumulator: Impact, element: EFRecord -> accumulator.factor(element) })
 
         // when
         val actual = substances.values.groupingBy { it.lcaFileName }
@@ -91,18 +87,18 @@ class StdLibTest {
                     compartment = "soil"
                     sub_compartment = "non-agricultural"
                     reference_unit = kg
-                    
+                
                     impacts {
                         2.0522E-08 u Human_toxicity_non_cancer
-                        2.0522E-08 u Human_toxicity_non_cancer_organics    
+                        2.0522E-08 u Human_toxicity_non_cancer_organics
                     }
-                    
+                
                     meta {
                         type = "emissions"
                         generator = "kleis-lca-generator"
                         casNumber = "1404190-37-9"
                         ecNumber = "801-941-7"
-                    }    
+                    }
                 
                 }
                 
@@ -111,13 +107,59 @@ class StdLibTest {
         assertEquals(expected, actual[key])
     }
 
-    private fun loadRecords(): Sequence<CSVRecord> {
+    @Test
+    fun substance30() {
+        // given
+        val substances = loadRecords("30")
+            .map { EF30Record(it) }
+            .groupingBy { it.substanceId() }
+            .fold({ _: String, element: EFRecord -> Impact() },
+                { _: String, accumulator: Impact, element: EFRecord -> accumulator.factor(element) })
+
+        // when
+        val actual = substances.values.groupingBy { it.lcaFileName }
+            .fold("package ef30\n\n") { accumulator: String, element: Impact ->
+                accumulator.plus(element.fileContent).plus("\n\n")
+            }
+
+        // then
+        val key = "_3ar_5as_9as_9br_3a_6_6_9a_tetramethyldodecahydronaphtho_2_1_b_furan"
+        val expected = """
+                package ef30
+
+                substance _3ar_5as_9as_9br_3a_6_6_9a_tetramethyldodecahydronaphtho_2_1_b_furan_water {
+
+                    name = "(-)-(3ar,5as,9as,9br)-3a,6,6,9a-tetramethyldodecahydronaphtho[2,1-b]furan"
+                    compartment = "water"
+
+                    reference_unit = kg
+
+                    impacts {
+                        1199.7 u Ecotoxicity_freshwater
+                        1199.7 u Ecotoxicity_freshwater_organics
+                    }
+                
+                    meta {
+                        type = "emissions"
+                        generator = "kleis-lca-generator"
+                        casNumber = "6790-58-5"
+                        ecNumber = "229-861-2"
+                    }
+
+                }
+                
+                
+                """.trimIndent()
+        assertEquals(expected, actual[key])
+    }
+
+    private fun loadRecords(version: String): Sequence<CSVRecord> {
         val csvFormat = CSVFormat.Builder.create().setHeader().build()
-        val flowInput = FileReader("src/test/resources/flows.csv")
+        val flowInput = FileReader("src/test/resources/flows$version.csv")
         val flows = csvFormat.parse(flowInput).asSequence()
 
 
-        val factorsInput = FileReader("src/test/resources/factors.csv")
+        val factorsInput = FileReader("src/test/resources/factors$version.csv")
         val factors = csvFormat.parse(factorsInput).asSequence()
         return flows.plus(factors)
     }
