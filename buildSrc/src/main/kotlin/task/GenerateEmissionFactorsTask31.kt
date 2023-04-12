7c1de0b191a500b430d539ba8f9b2088928d55cb
Philippe Kernevez <philippe@kernevez.net>
2023-04-12 15:09:16 +0200

    Import ef30

---
+++ buildSrc/src/main/kotlin/task/GenerateEmissionFactorsTask31.kt
@@ -0,0 +1,74 @@
+package task
+
+import net.lingala.zip4j.io.outputstream.ZipOutputStream
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import org.apache.commons.csv.CSVRecord
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.*
+import org.gradle.work.Incremental
+import org.gradle.work.InputChanges
+import java.io.FileOutputStream
+import java.nio.charset.Charset.defaultCharset
+import java.util.zip.GZIPInputStream
+import kotlin.streams.asSequence
+
+abstract class GenerateEmissionFactorsTask31 : DefaultTask() {
+
+    @get:Incremental
+    @get:PathSensitive(PathSensitivity.NAME_ONLY)
+    @get:InputDirectory
+    abstract val inputDir: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val outputDir: DirectoryProperty
+
+    private val csvFormat = CSVFormat.Builder.create().setHeader().build()
+
+    init {
+        group = "ch.kleis"
+        description = "generateEmissionFactors3_1"
+        inputDir.convention(this.project.layout.projectDirectory.dir("src/main/stdlib/ef3.1"))
+        outputDir.convention(this.project.layout.buildDirectory.dir("stdlib/ef3.1"))
+    }
+
+    @TaskAction
+    fun execute(inputChanges: InputChanges) {
+
+        val outputFile = outputDir.file("emissions_factors3.1.jar").get().asFile
+        val outputJarStream = ZipOutputStream(FileOutputStream(outputFile))
+
+        val substances = loadAllRecords()
+            .map { EF31Record(it) }
+            .groupingBy { it.substanceId() }
+            .fold({ _: String, element: EF31Record -> Impact() },
+                { _: String, accumulator: Impact, element: EF31Record -> accumulator.factor(element) })
+
+        substances.values.groupingBy { it.lcaFileName }
+            .fold("package ef31\n\n") { accumulator: String, element: Impact ->
+                accumulator.plus(element.fileContent).plus("\n\n")
+            }
+            .forEach { entry -> generateZipEntry(outputJarStream, entry.key, entry.value) }
+
+        outputJarStream.close()
+    }
+
+
+    private fun loadAllRecords(): Sequence<CSVRecord> {
+        val flowCSVParser = CSVParser.parse(
+            GZIPInputStream(
+                inputDir.file("flows.31.csv.gz")
+                    .map { it.asFile }.get().inputStream()
+            ), defaultCharset(), csvFormat
+        )
+        val factorsCSVParser = CSVParser.parse(
+            GZIPInputStream(
+                inputDir.file("factors.31.csv.gz")
+                    .map { it.asFile }.get().inputStream()
+            ), defaultCharset(), csvFormat
+        )
+        return (flowCSVParser.stream().asSequence() +
+                factorsCSVParser.stream().asSequence())
+    }
+}
