c52c4953abb2ccebebb31d6862dde70ef623f889
Peva Blanchard <pblanchard@kleis.ch>
2023-05-02 14:35:05 +0200

    psi substance spec : field getters

--- src/main/kotlin/ch/kleis/lcaplugin/language/psi/type/spec/PsiSubstanceSpec.kt
+++ src/main/kotlin/ch/kleis/lcaplugin/language/psi/type/spec/PsiSubstanceSpec.kt
@@ -1,5 +1,6 @@
 package ch.kleis.lcaplugin.language.psi.type.spec
 
+import ch.kleis.lcaplugin.language.psi.type.field.PsiStringLiteralField
 import ch.kleis.lcaplugin.language.psi.type.ref.PsiSubstanceRef
 import ch.kleis.lcaplugin.language.psi.type.trait.PsiUIDOwner
 import ch.kleis.lcaplugin.psi.LcaTypes
@@ -7,5 +8,17 @@ import ch.kleis.lcaplugin.psi.LcaTypes
 interface PsiSubstanceSpec : PsiUIDOwner {
     fun getSubstanceRef(): PsiSubstanceRef {
         return node.findChildByType(LcaTypes.SUBSTANCE_REF)?.psi as PsiSubstanceRef
+    }
+
+    fun getTypeField(): PsiStringLiteralField? {
+        return node.findChildByType(LcaTypes.TYPE_FIELD)?.psi as PsiStringLiteralField?
+    }
+
+    fun getCompartmentField(): PsiStringLiteralField? {
+        return node.findChildByType(LcaTypes.COMPARTMENT_FIELD)?.psi as PsiStringLiteralField?
+    }
+
+    fun getSubCompartmentField(): PsiStringLiteralField? {
+        return node.findChildByType(LcaTypes.SUB_COMPARTMENT_FIELD)?.psi as PsiStringLiteralField?
     }
 }
