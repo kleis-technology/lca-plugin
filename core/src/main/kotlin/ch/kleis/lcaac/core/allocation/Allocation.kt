884afd1a06b1646c96aab7d32baf5d6a289c1d85
Arnaud Béguin <abeguin@kleis.ch>
2023-09-29 12:00:23 +0200

    add copyright notice in core and grammar packages

--- core/src/main/kotlin/ch/kleis/lcaac/core/allocation/Allocation.kt
+++ core/src/main/kotlin/ch/kleis/lcaac/core/allocation/Allocation.kt
@@ -1,3 +1,10 @@
+/** 
+* Copyright (C) Kleis Technology Sàrl - All Rights Reserved 
+* Unauthorized copying of this file, via any medium is strictly prohibited 
+* Proprietary and confidential 
+* Written by Arnaud Béguin <abeguin@kleis.ch>, September 2023 
+*/ 
+
 package ch.kleis.lcaac.core.allocation
 
 import ch.kleis.lcaac.core.lang.dimension.UnitSymbol
--- core/src/main/kotlin/ch/kleis/lcaac/core/lang/expression/optics/LcaExpressionOptics.kt
+++ core/src/main/kotlin/ch/kleis/lcaac/core/lang/expression/optics/LcaExpressionOptics.kt
@@ -1,3 +1,10 @@
+/** 
+* Copyright (C) Kleis Technology Sàrl - All Rights Reserved 
+* Unauthorized copying of this file, via any medium is strictly prohibited 
+* Proprietary and confidential 
+* Written by Arnaud Béguin <abeguin@kleis.ch>, September 2023 
+*/ 
+
 @file:Suppress("NOTHING_TO_INLINE")
 
 package ch.kleis.lcaac.core.lang.expression.optics
