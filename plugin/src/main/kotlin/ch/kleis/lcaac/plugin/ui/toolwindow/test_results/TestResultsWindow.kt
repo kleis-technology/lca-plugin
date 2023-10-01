fcb54fc37d0a4565ad811ce499b5fccecab9e32a
Peva Blanchard <pblanchard@kleis.ch>
2023-10-01 11:03:15 +0200

    plugin: display 'No tests found' when empty results list

--- plugin/src/main/kotlin/ch/kleis/lcaac/plugin/ui/toolwindow/test_results/TestResultsWindow.kt
+++ plugin/src/main/kotlin/ch/kleis/lcaac/plugin/ui/toolwindow/test_results/TestResultsWindow.kt
@@ -3,6 +3,7 @@ package ch.kleis.lcaac.plugin.ui.toolwindow.test_results
 import ch.kleis.lcaac.plugin.testing.LcaTestResult
 import ch.kleis.lcaac.plugin.ui.toolwindow.LcaToolWindowContent
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.treeStructure.Tree
 import java.awt.BorderLayout
 import javax.swing.JPanel
@@ -10,18 +11,21 @@ import javax.swing.JPanel
 class TestResultsWindow(
     val results: List<LcaTestResult>
 ) : LcaToolWindowContent {
-    private val content: JPanel
+    private val content: JPanel = JPanel(BorderLayout())
 
     companion object {
         private val LOG = Logger.getInstance(TestResultsWindow::class.java)
     }
 
     init {
-        val model = TestResultTree(results)
-        val tree = Tree(model)
-        tree.addMouseListener(TestResultsTreeMouseListener)
-        content = JPanel(BorderLayout())
-        content.add(tree)
+        if (results.isNotEmpty()) {
+            val model = TestResultTree(results)
+            val tree = Tree(model)
+            tree.addMouseListener(TestResultsTreeMouseListener)
+            content.add(tree)
+        } else {
+            content.add(JBLabel("No tests found"), BorderLayout.PAGE_START)
+        }
         content.updateUI()
     }
 
