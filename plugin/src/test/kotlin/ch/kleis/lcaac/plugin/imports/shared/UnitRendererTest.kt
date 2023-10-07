989175ab92154716ea49d068b81579b495f029ee
Peva Blanchard <pblanchard@kleis.ch>
2023-10-07 22:52:40 +0200

    plugin: unit manager tests, unit renderer tests

--- plugin/src/test/kotlin/ch/kleis/lcaac/plugin/imports/shared/UnitRendererTest.kt
+++ plugin/src/test/kotlin/ch/kleis/lcaac/plugin/imports/shared/UnitRendererTest.kt
@@ -1,14 +1,13 @@
 package ch.kleis.lcaac.plugin.imports.shared
 
 import ch.kleis.lcaac.plugin.imports.ModelWriter
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.slot
-import io.mockk.unmockkAll
+import ch.kleis.lcaac.plugin.imports.model.ImportedUnit
+import ch.kleis.lcaac.plugin.imports.shared.serializer.UnitSerializer
+import io.mockk.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.fail
+import kotlin.test.assertEquals
 
 class UnitRendererTest {
     private val writer = mockk<ModelWriter>()
@@ -26,7 +25,36 @@ class UnitRendererTest {
     }
 
     @Test
-    fun testMe() {
-        fail("test me")
+    fun render_whenNewUnit_shouldWrite() {
+        // given
+        val manager = UnitManager()
+        val importedUnit = ImportedUnit("dimension", "symbol")
+        val serializer = mockk<UnitSerializer>()
+        every { serializer.serialize(any()) } returns "this is the body"
+        val renderer = UnitRenderer(manager, serializer)
+
+        // when
+        renderer.render(importedUnit, writer)
+
+        // then
+        assertEquals("unit", pathSlot.captured)
+        assertEquals("this is the body", bodySlot.captured)
+    }
+
+    @Test
+    fun render_whenKnownUnit_shouldNotWrite() {
+        // given
+        val manager = UnitManager()
+        val importedUnit = ImportedUnit("dimension", "symbol")
+        manager.add(importedUnit)
+        val serializer = mockk<UnitSerializer>()
+        every { serializer.serialize(any()) } returns "this is the body"
+        val renderer = UnitRenderer(manager, serializer)
+
+        // when
+        renderer.render(importedUnit, writer)
+
+        // then
+        verify(exactly = 0) { writer.writeAppendFile(any(), any()) }
     }
 }
