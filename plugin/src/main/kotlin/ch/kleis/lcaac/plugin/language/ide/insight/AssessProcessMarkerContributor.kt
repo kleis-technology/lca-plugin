8baca39342193bc4296aea3c2c2b679fc638f8d3
Philippe Kernevez <philippe@kernevez.net>
2023-10-09 18:33:41 +0200

    Add Generate with CSV Action

--- plugin/src/main/kotlin/ch/kleis/lcaac/plugin/language/ide/insight/AssessProcessMarkerContributor.kt
+++ plugin/src/main/kotlin/ch/kleis/lcaac/plugin/language/ide/insight/AssessProcessMarkerContributor.kt
@@ -2,11 +2,12 @@ package ch.kleis.lcaac.plugin.language.ide.insight
 
 import ch.kleis.lcaac.plugin.actions.AssessProcessAction
 import ch.kleis.lcaac.plugin.actions.ContributionAnalysisWithDataAction
+import ch.kleis.lcaac.plugin.actions.ModelGenerationWithDataAction
 import ch.kleis.lcaac.plugin.actions.SensitivityAnalysisAction
 import ch.kleis.lcaac.plugin.actions.sankey.SankeyGraphAction
 import ch.kleis.lcaac.plugin.language.psi.isProcess
-import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import ch.kleis.lcaac.plugin.psi.LcaProcess
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
 import com.intellij.psi.PsiElement
 
@@ -18,10 +19,11 @@ class AssessProcessMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
         if (isProcess(element)) {
             val process = element.parent as LcaProcess
-            val target = process.getProcessRef().getUID().name
+            val target = process.processRef.getUID().name
             val labels = process.getLabels()
             val assessProcessAction = AssessProcessAction(target, labels)
-            val assessProcessWithExternalDataAction = ContributionAnalysisWithDataAction(target, labels)
+            val assessProcessWithExternalDataActionCsv = ContributionAnalysisWithDataAction(target, labels)
+            val generateModelWithExternalDataAction = ModelGenerationWithDataAction(target, process)
             val sankeyGraphAction = SankeyGraphAction(target, labels)
             val sensitivityAnalysisAction = SensitivityAnalysisAction(target, labels)
             return Info(
@@ -30,7 +32,8 @@ class AssessProcessMarkerContributor : RunLineMarkerContributor() {
                     "Run $target"
                 },
                 assessProcessAction,
-                assessProcessWithExternalDataAction,
+                assessProcessWithExternalDataActionCsv,
+                generateModelWithExternalDataAction,
                 sankeyGraphAction,
                 sensitivityAnalysisAction,
             )
