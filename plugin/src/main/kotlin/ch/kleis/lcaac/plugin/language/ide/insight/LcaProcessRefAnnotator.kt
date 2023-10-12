e433927975223febdb345db61722f83243a8e5b8
Philippe Kernevez <philippe@kernevez.net>
2023-10-12 23:39:46 +0200

    part1

---
+++ plugin/src/main/kotlin/ch/kleis/lcaac/plugin/language/ide/insight/LcaProcessRefAnnotator.kt
@@ -0,0 +1,31 @@
+package ch.kleis.lcaac.plugin.language.ide.insight
+
+import ch.kleis.lcaac.plugin.language.ide.insight.AnnotatorHelper.annotateErrWithMessage
+import ch.kleis.lcaac.plugin.psi.LcaProcess
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.psi.PsiElement
+
+class LcaProcessRefAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        if (element !is LcaProcess) {
+            return
+        }
+
+        val products = element.getProducts()
+        if (products.size <= 1) {
+            return
+        }
+
+        val productNames = products.map { it.outputProductSpec.name }
+        val productsWithoutAllocationFactors = products
+            .filter { it.outputProductSpec.allocateField == null }
+        if (productsWithoutAllocationFactors.isNotEmpty()) {
+            annotateErrWithMessage(
+                element.blockProductsList.first(),
+                holder,
+                "some products in $productNames are missing allocation factors",
+            )
+        }
+    }
+}
---
+++ plugin/src/test/kotlin/ch/kleis/lcaac/plugin/language/ide/insight/LcaProcessRefAnnotatorTest.kt
@@ -0,0 +1,12 @@
+package ch.kleis.lcaac.plugin.language.ide.insight
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class LcaProcessRefAnnotatorTest : BasePlatformTestCase() {
+
+
+}
