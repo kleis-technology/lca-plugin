44ade00a1c9f8c3b11a73d909a34cf11f01b5456
Joachim Desroches <jdesroches@kleis.ch>
2023-11-02 15:23:15 +0100

    Round instead of truncating Double's for display.

--- plugin/src/main/kotlin/ch/kleis/lcaac/plugin/ui/toolwindow/shared/QuantityRenderer.kt
+++ plugin/src/main/kotlin/ch/kleis/lcaac/plugin/ui/toolwindow/shared/QuantityRenderer.kt
@@ -1,11 +1,17 @@
 package ch.kleis.lcaac.plugin.ui.toolwindow.shared
 
+import java.text.DecimalFormat
 import javax.swing.table.DefaultTableCellRenderer
 
 object QuantityRenderer : DefaultTableCellRenderer() {
+    private val formatter = DecimalFormat("0.##E0")
+
+    fun formatDouble(value: Double): String =
+        formatter.format(value + 0.0).removeSuffix("E0")
+
     override fun setValue(value: Any?) {
         when (value) {
-            is Double -> text = FloatingPointRepresentation.of(value).toString()
+            is Double -> text = formatDouble(value)
             else -> super.setValue(value)
         }
     }
