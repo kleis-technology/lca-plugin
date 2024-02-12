1c0fb01455cd4fdc8d7044f84b984014fae05abe
Peva Blanchard <pblanchard@kleis.ch>
2024-02-12 23:30:12 +0100

    core bump version 1.6.3

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -47,7 +47,7 @@ sourceSets {
 }
 
 dependencies {
-    implementation("ch.kleis.lcaac:core:1.6.0")
+    implementation("ch.kleis.lcaac:core:1.6.3")
 
     implementation(files(layout.buildDirectory.dir("stdlib/ef3.1")) {
         builtBy("generateEmissionFactors31")
