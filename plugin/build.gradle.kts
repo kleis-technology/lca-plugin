1196e8a1b7a0b36ee8a6933a2c9e16af604fb6df
Peva Blanchard <pblanchard@kleis.ch>
2024-06-13 23:30:54 +0200

    updated core 1.6.6

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -47,7 +47,7 @@ sourceSets {
 }
 
 dependencies {
-    implementation("ch.kleis.lcaac:core:1.6.5")
+    implementation("ch.kleis.lcaac:core:1.6.6")
 
     implementation(files(layout.buildDirectory.dir("stdlib/ef3.1")) {
         builtBy("generateEmissionFactors31")
