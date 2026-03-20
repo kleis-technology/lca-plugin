56a26f9bea1d6dd996dc65169169b0555716a9a8
Peva Blanchard <pblanchard@kleis.ch>
2026-03-20 09:55:31 +0100

    csv processor: include global parameters

--- plugin/src/main/kotlin/ch/kleis/lcaac/plugin/actions/csv/CsvProcessor.kt
+++ plugin/src/main/kotlin/ch/kleis/lcaac/plugin/actions/csv/CsvProcessor.kt
@@ -32,32 +32,22 @@ class CsvProcessor(
         )
     )
     private val sourceOps = DefaultDataSourceOperations(ops, config, factory.buildConnectors())
-    private val evaluator = Evaluator(symbolTable, ops, sourceOps)
 
     fun process(request: CsvRequest): List<CsvResult> {
+        val globalParameters = parseOverrides(
+            symbolTable.globalParameters.toMap().mapKeys { it.key.name },
+            request,
+        )
         val reqName = request.processName
         val reqLabels = request.matchLabels
         val template =
             symbolTable.getTemplate(reqName, reqLabels)
                 ?: throw EvaluatorException("Could not get template for ${request.processName}")
+        val processParameters = parseOverrides(template.params, request)
 
-        val arguments = template.params
-            .mapValues { entry ->
-                when (val v = entry.value) {
-                    is QuantityExpression<*> -> request[entry.key]?.let {
-                        val amount = parseDouble(it)
-                        EQuantityScale(ops.pure(amount), EUnitOf(v))
-                    } ?: entry.value
-
-                    is StringExpression -> request[entry.key]?.let {
-                        EStringLiteral(it)
-                    } ?: entry.value
-
-                    else -> throw EvaluatorException("$v is not a supported data expression")
-                }
-            }
-
-        val trace = evaluator.trace(template, arguments)
+        val st = symbolTable.overrideGlobalParameters(globalParameters)
+        val evaluator = Evaluator(st, ops, sourceOps)
+        val trace = evaluator.trace(template, processParameters)
         val systemValue = trace.getSystemValue()
         val entryPoint = trace.getEntryPoint()
         val program = ContributionAnalysisProgram(systemValue, entryPoint)
@@ -73,4 +63,23 @@ class CsvProcessor(
                 )
             }
     }
+
+    private fun parseOverrides(
+        data: Map<String, DataExpression<BasicNumber>>,
+        request: CsvRequest
+    ): Map<String, DataExpression<BasicNumber>> = data
+        .mapValues { entry ->
+            when (val v = entry.value) {
+                is QuantityExpression<*> -> request[entry.key]?.let {
+                    val amount = parseDouble(it)
+                    EQuantityScale(ops.pure(amount), EUnitOf(v))
+                } ?: entry.value
+
+                is StringExpression -> request[entry.key]?.let {
+                    EStringLiteral(it)
+                } ?: entry.value
+
+                else -> throw EvaluatorException("$v is not a supported data expression")
+            }
+        }
 }
